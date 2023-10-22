da6329f1f4dc6dcdd84b8855cf3d1b8fb8b551ba
Allyson Alves <allysonalves.01@gmail.com>
2023-10-22 18:00:58 -0300

    Clone da PSN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/cards/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    BannerComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
